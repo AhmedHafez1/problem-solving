81db9234f99ef7f65b5b52a8c6286a21791e8988
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-01-15 14:03:52 +0200

    Remove Duplicates from Sorted Array

---
+++ Remove Duplicates from Sorted Array/index.ts
@@ -0,0 +1,18 @@
+function removeDuplicates(nums: number[]): number {
+  let i = 0;
+
+  while (i < nums.length) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return nums.length;
+}
+
+const a1 = [1, 3, 5, 6, 6, 8, 9, 9, 9, 10];
+const k = removeDuplicates(a1);
+
+console.log(k);
